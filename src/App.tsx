25b6472bd4488087dcce2720a7bad41cfbcf6d8e
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-06-14 07:23:04 +0800

    Add overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Overlay, Task } from "./Overlay";
+
 function App() {
-  return <>Testing</>
+  // const params = new URL(location.href).searchParams;
+  // const clientID = params.get('c');
+  // const authToken = params.get('t');
+  const tasks: Task[] = [{
+    id: 1,
+    name: 'Do something',
+    username: 'SpaceMonkeyJT',
+    addedDate: Date.now(),
+  }];
+  return <Overlay tasks={tasks} />
 }
 
 export default App

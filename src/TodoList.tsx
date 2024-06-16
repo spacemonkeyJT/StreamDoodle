674212d8403d40307a2cee7cc70ccf352e73532c
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-06-17 06:11:25 +0800

    Show list background when empty

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -6,20 +6,17 @@ interface Props {
 }
 
 export function TodoList({ tasks }: Props) {
-  if (tasks.length > 0) {
-    return (
-      <div className="todo-list">
-        {tasks
-          .sort((a, b) => a.addedDate - b.addedDate)
-          .map(t => (
-            <div className="task" key={t.id}>
-              <div className="task-date">{new Date(t.addedDate).toLocaleTimeString()}</div>
-              <div className="task-username">{t.username}</div>
-              <div className="task-name">{t.name}</div>
-            </div>
-          ))}
-      </div>
-    )
-  }
-  return null
+  return (
+    <div className="todo-list">
+      {tasks
+        .sort((a, b) => a.addedDate - b.addedDate)
+        .map(t => (
+          <div className="task" key={t.id}>
+            <div className="task-date">{new Date(t.addedDate).toLocaleTimeString()}</div>
+            <div className="task-username">{t.username}</div>
+            <div className="task-name">{t.name}</div>
+          </div>
+        ))}
+    </div>
+  )
 }

d1e168073cf53ab30f6d4ea0e270ba68763ea2ea
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-08-03 20:38:12 +0800

    Split client and server folders

--- server/server.cjs
+++ server/server.cjs
@@ -13,12 +13,12 @@ app.get('/api', (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(port, () => {

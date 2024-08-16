396462e9c84abc94490b25d8aa55b8e4901865cf
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-08-16 19:20:00 +0800

    Fix login

--- src/loaders/rootData.ts
+++ src/loaders/rootData.ts
@@ -1,4 +1,4 @@
-import { redirect, useRouteLoaderData } from "react-router-dom"
+import { useRouteLoaderData } from "react-router-dom"
 import { Tables } from "../database.types"
 import { AuthInfo, getAuthInfo } from "../utils/auth"
 import { supabase } from "../utils/supabase"
@@ -9,14 +9,10 @@ export interface RootData {
 }
 
 export async function loadRootData() {
-  const auth = getAuthInfo()
-  if (auth) {
-    return {
-      auth,
-      channels: (await supabase.from('channels').select()).data,
-    }
+  return {
+    auth: getAuthInfo(),
+    channels: (await supabase.from('channels').select()).data,
   }
-  return redirect('/login')
 }
 
 export function useRootData() {

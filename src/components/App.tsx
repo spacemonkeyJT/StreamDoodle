4ccbdd24e4d9144be19fd6f6cec6a43000248821
SpaceMonkeyJT <spacemonkeyjt1@gmail.com>
2024-08-16 12:28:58 +0800

    Add logout button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom"
+import { supabase } from "../db"
+
+async function logout() {
+  const { error } = await supabase.auth.signOut()
+  if (error) {
+    console.error(error)
+  } else {
+    location.href = '/login'
+  }
+}
 
 function App() {
   console.log(JSON.parse(localStorage.getItem('sb-knnmxxukaxfrhabsstvh-auth-token') ?? '{}'))
   return <div>
     App root
+    <button onClick={logout}>Log out</button>
     <Outlet />
   </div>
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,27 +5,6 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { supabase } from "../db"
 
-// async function tryLogin(access_token: string | undefined | null) {
-//   if (access_token) {
-//     if (!localStorage.getItem('access_token')) {
-//       log(`Storing access token: ${access_token}`);
-//       localStorage.setItem('access_token', access_token);
-//     }
-//     try {
-//       log(`Attempting to login with access token: ${access_token}`);
-//       const { token } = await apiLoginTwitch(access_token);
-//       log('Login successful');
-//       Cookies.set('token', token);
-//       return true;
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-//   return false;
-// }
-
-
-
 export default function Login() {
   const [session, setSession] = useState<Session | null>(null)
 
@@ -50,29 +29,15 @@ export default function Login() {
   }, [])
 
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={["twitch"]} onlyThirdPartyProviders />)
+    return (
+      <div className="login">
+        <h2>streamdoodle</h2>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={["twitch"]} onlyThirdPartyProviders />
+      </div>
+    )
   }
   else {
+    location.href = '/'
     return (<div>Logged in!</div>)
   }
-
-  // const token = Cookies.get('token');
-  // const access_token = localStorage.getItem('access_token');
-
-  // useEffect(() => {
-  //   (async () => {
-  //     if ((token && access_token) || await tryLogin(getLocationHash().access_token) || await tryLogin(access_token)) {
-  //       log('Logged in, navigating to site root');
-  //       location.href = '/';
-  //     }
-  //   })();
-  // }, [])
-
-  // return <div className="login">
-  //   <h1>streamdoodle</h1>
-  //   <a className="login-twitch" href={twitchLoginUrl}>
-  //     <img className="logo" src={TwitchLogo} />
-  //     <div className="label">Login with Twitch</div>
-  //   </a>
-  // </div>
 }
